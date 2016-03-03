5a0d43974f3955508cf65368bfd000307c7a152b
Cooper <674027158@qq.com>
2016-03-03 21:51:29 +0800

    Initial Commit

---
+++ CPLikeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CPLikeDemo
+//
+//  Created by Cooper on 3/3/16.
+//  Copyright © 2016 Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CPLikeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CPLikeDemo
+//
+//  Created by Cooper on 3/3/16.
+//  Copyright © 2016 Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
