cb7c6baac07c62667b6264264f7a3655202f9440
Cooper <674027158@qq.com>
2016-07-25 17:19:36 +0800

    代码从项目中更新过来，解决了导致内存泄露的

--- CPLikeDemo/CPLikeAnimator/CPLikeAnimator.m
+++ CPLikeDemo/CPLikeAnimator/CPLikeAnimator.m
@@ -8,26 +8,33 @@
 
 #import "CPLikeAnimator.h"
 
-static const CGFloat likeImgWidth            = 30.0;
-static const CGFloat likeImgHeight           = 25.0;
-static const CFTimeInterval animationTime    = 4.0;
+static const CGFloat likeImgWidth         = 36;
+static const CGFloat likeImgHeight        = 32;
+static const CFTimeInterval animationTime = 4.0;
 
-static const int numOfColor                  = 4;
-static const NSInteger animateMaxHeight      = 400;
+static const CGFloat xTranslationWidth    = 81;
+static const CGFloat xTranslateOffset     = (xTranslationWidth - likeImgWidth) / 2.0;
 
-static NSString *const kXDLiveLikeAnimations = @"kCPLikeAnimations";
+static const int numOfColor                  = 14;
+static const NSInteger animateMaxHeight      = 300;
+
+static NSString *const kXDLiveLikeAnimations = @"kXDLiveLikeAnimations";
 static NSString *const kForRemoveLayer       = @"kForRemoveLayer";
 
+#pragma mark -
+#pragma mark ------------ Utils ------------
+
+static int _random(int min, int max) {
+    return arc4random() % (max - min + 1) + min;
+}
+
 @interface CPLikeAnimator()
-
-@property (nonatomic, strong) UIView *hostView;
-
 @end
 
 @implementation CPLikeAnimator
 
 #pragma mark -
-#pragma mark ------------ Public Properties ------------
+#pragma mark ------------ Public Functions ------------
 
 + (CGPoint) converCenterPointToWindow:(UIView*)view {
     UIWindow *window = [[[UIApplication sharedApplication] delegate] window];
@@ -36,34 +43,38 @@ static NSString *const kForRemoveLayer       = @"kForRemoveLayer";
 
 - (void) animateOnView:(UIView*) view atPoint:(CGPoint) point {
     
-    self.hostView = view;
-    
-    CALayer *likeLayer = [self likeLayer];
+    CALayer *likeLayer = [self p_likeLayer];
     likeLayer.position = point;
     [view.layer addSublayer:likeLayer];
     
-    CAAnimationGroup *ani = [self animations];
+    CAAnimationGroup *ani = [self p_animations];
     ani.removedOnCompletion = NO;
     ani.fillMode = kCAFillModeForwards;
+    ani.delegate = self;
     [ani setValue:likeLayer forKey:kForRemoveLayer];
     
     [likeLayer addAnimation:ani forKey:kXDLiveLikeAnimations];
-    
-    [likeLayer performSelector:@selector(removeFromSuperlayer) withObject:nil afterDelay:animationTime+0.1];
-    
+}
+
+- (void)dealloc {
     
 }
 
 #pragma mark -
 #pragma mark ------------ CAAnimationDelegate ------------
-
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+    CALayer *layer = [anim valueForKey:kForRemoveLayer];
+    [layer removeAllAnimations];
+    [layer removeFromSuperlayer];
+    layer = nil;
+}
 
 #pragma mark -
 #pragma mark ------------ Private Functions ------------
-- (CALayer*) likeLayer {
+- (CALayer*) p_likeLayer {
     
     CALayer *layer = [[CALayer alloc] init];
-
+    
     int index = arc4random() % numOfColor;
     
     layer.bounds = CGRectMake(0, 0,likeImgWidth ,likeImgHeight);
@@ -76,7 +87,7 @@ static NSString *const kForRemoveLayer       = @"kForRemoveLayer";
     
 }
 
-- (CAAnimationGroup *) animations {
+- (CAAnimationGroup *) p_animations {
     
     //渐变
     CABasicAnimation *fade = [CABasicAnimation new];
@@ -88,14 +99,14 @@ static NSString *const kForRemoveLayer       = @"kForRemoveLayer";
     //x
     CAKeyframeAnimation *x = [CAKeyframeAnimation new];
     x.keyPath = @"transform.translation.x";
-    x.values = [self valuesForXAnimation];//@[@(-15),@(15),@(-15),@(15)];
+    x.values = [self p_valuesForXAnimation];//@[@(-15),@(15),@(-15),@(15)];
     x.duration = animationTime;
     x.additive = YES;
     
     //y
     CAKeyframeAnimation *y = [CAKeyframeAnimation new];
     y.keyPath = @"transform.translation.y";
-    y.values = [self valuesForYAnimation];//@[@(0),@(-100),@(-150),@(-200)];
+    y.values = [self p_valuesForYAnimation];//@[@(0),@(-100),@(-150),@(-200)];
     y.duration = animationTime;
     y.additive = YES;
     
@@ -110,47 +121,43 @@ static NSString *const kForRemoveLayer       = @"kForRemoveLayer";
     CAAnimationGroup *group = [[CAAnimationGroup alloc] init];
     group.animations = @[ scale,fade,x,y ];
     group.duration = animationTime;
-
+    
     return group;
 }
 
-- (int) randomIntFrom:(int) min to:(int) max {
-    return arc4random() % (max - min + 1) + min;
-}
-
-- (NSArray<NSNumber *> *) valuesForXAnimation {
+- (NSArray<NSNumber *> *) p_valuesForXAnimation {
     
     NSMutableArray *array = [[NSMutableArray<NSNumber *> alloc] init];
     
-    int arraySize = [self randomIntFrom:5 to:8] - 1;
-    int firstValue = [self randomIntFrom:-15 to:15];
+    int arraySize = _random(5,8) - 1;
+    int firstValue = _random(-xTranslateOffset,xTranslateOffset);
     
     [array addObject:@(firstValue)];
     
     for (int i = 1; i < arraySize; i ++) {
-        [array addObject:@([self randomIntFrom:-15 to:15])];
+        [array addObject:@(_random(-xTranslateOffset,xTranslateOffset))];
     }
     
-    return [array copy];
+    return [array mutableCopy];
 }
 
-- (NSArray<NSNumber *> *) valuesForYAnimation {
+- (NSArray<NSNumber *> *) p_valuesForYAnimation {
     
     NSMutableArray *array = [[NSMutableArray<NSNumber *> alloc] init];
     
-    NSInteger arraySize = [self randomIntFrom:3 to:4];
+    NSInteger arraySize = _random(3, 4);
     
     int lastValue = 0;
     for (int i = 0; i < arraySize; i ++) {
         
         int root = (animateMaxHeight/(arraySize*1.0)) * (i+1);
-        int max = root + [self randomIntFrom:0 to:10];
+        int max = root + _random(0, 10);
         
-        int newValue = [self randomIntFrom:lastValue to:max] * -1;
+        int newValue = _random(lastValue, max) * -1;
         lastValue = -newValue;
         [array addObject:@(newValue)];
     }
     
-    return [array copy];
+    return [array mutableCopy];
 }
 @end
